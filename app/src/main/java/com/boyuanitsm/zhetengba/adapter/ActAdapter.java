1acb3349767654cb533eb2ddaf086b7ce845bcb2
小可 <164454216@qq.com>
2016-05-04 15:17:17 +0800

    //设置首页控件点击事件

--- app/src/main/java/com/boyuanitsm/zhetengba/adapter/ActAdapter.java
+++ app/src/main/java/com/boyuanitsm/zhetengba/adapter/ActAdapter.java
@@ -4,16 +4,33 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.boyuanitsm.zhetengba.R;
 
 /**
- * 活动listview适配器
+ * 活动listview适配器.
+ * 设置条目点击
  * Created by xiaoke on 2016/4/25.
  */
-public class ActAdapter extends BaseAdapter {
+public class ActAdapter extends BaseAdapter implements View.OnClickListener{
 /*    private List<Person> persons;*/
     private Context context;
+    private View view;
+
+    private LinearLayout ll_person;//个人信息id
+    private TextView tv_hdtheme;//活动主题
+    private TextView tv_loaction;//活动位置
+    private TextView tv_date;//活动日期
+    private LinearLayout ll_guanzhu;//关注数量
+    private ImageView iv_guanzhu;//关注图标
+    private TextView tv_guanzhu_num;//关注数量设置
+    private LinearLayout ll_join;//参加人数
+    private ImageView iv_join;//参加头像
+    private TextView tv_join_num;//参加数量
+    private TextView tv_join_tal_num;//活动总人数设置
 
     public ActAdapter(Context context) {
         this.context=context;
@@ -37,7 +54,6 @@ public class ActAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View view;
 /*
         Person person = persons.get(position);
         final Holder viewHolder;
@@ -70,9 +86,34 @@ public class ActAdapter extends BaseAdapter {
         viewHolder.iv_gender.setImageDrawable(Drawable.createFromPath(person.getHeadPhoto()));
 */
             view=View.inflate(context, R.layout.item_act,null);
-
+            init();
 
         return view;
+    }
+
+    /***
+     * 初始化条目控件
+     */
+    private void init() {
+        ll_person = (LinearLayout) view.findViewById(R.id.ll_person);
+        tv_hdtheme = (TextView) view.findViewById(R.id.tv_hdtheme);
+        tv_loaction= (TextView) view.findViewById(R.id.tv_loaction);
+        tv_date= (TextView) view.findViewById(R.id.tv_date);
+        ll_guanzhu= (LinearLayout) view.findViewById(R.id.ll_guanzhu);
+        iv_guanzhu= (ImageView) view.findViewById(R.id.iv_guanzhu);
+        tv_guanzhu_num= (TextView) view.findViewById(R.id.tv_guanzhu_num);
+        ll_join= (LinearLayout) view.findViewById(R.id.ll_join);
+        tv_loaction= (TextView) view.findViewById(R.id.tv_loaction);
+        iv_join = (ImageView) view.findViewById(R.id.iv_join);
+        tv_join_num= (TextView) view.findViewById(R.id.tv_join_num);
+        tv_join_tal_num= (TextView) view.findViewById(R.id.tv_join_tal_num);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        
     }
 
 /*    class Holder {
