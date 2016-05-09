e6eb0dd6caba3a8feab5983d44027bc4b7e5f63f
小可 <164454216@qq.com>
2016-05-09 11:51:27 +0800

    //添加下拉刷新

--- app/src/main/java/com/boyuanitsm/zhetengba/fragment/CirFrg.java
+++ app/src/main/java/com/boyuanitsm/zhetengba/fragment/CirFrg.java
@@ -1,5 +1,7 @@
 package com.boyuanitsm.zhetengba.fragment;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -11,6 +13,8 @@ import android.widget.ListView;
 
 import com.boyuanitsm.zhetengba.R;
 import com.boyuanitsm.zhetengba.adapter.CircleAdapter;
+import com.boyuanitsm.zhetengba.utils.ZtinfoUtils;
+import com.boyuanitsm.zhetengba.view.refresh.PullToRefreshListView;
 
 /**
  * 子界面-圈子界面
@@ -21,9 +25,17 @@ public class CirFrg extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.cir_frg, null);
-        ListView lv_cir = (ListView) view.findViewById(R.id.lv_cir);
+        PullToRefreshListView lv_cir = (PullToRefreshListView) view.findViewById(R.id.lv_cir);
         CircleAdapter adapter=new CircleAdapter(getContext());
-        lv_cir.setAdapter(adapter);
+        lv_cir.setPullRefreshEnabled(true);//下拉刷新
+        lv_cir.setScrollLoadEnabled(true);//滑动加载
+        lv_cir.setPullLoadEnabled(false);//上拉刷新
+        lv_cir.setHasMoreData(true);//是否有更多数据
+        lv_cir.getRefreshableView().setVerticalScrollBarEnabled(false);//设置右侧滑动
+        lv_cir.getRefreshableView().setSelector(new ColorDrawable(Color.TRANSPARENT));
+        lv_cir.setLastUpdatedLabel(ZtinfoUtils.getCurrentTime());
+        lv_cir.getRefreshableView().setDivider(null);
+        lv_cir.getRefreshableView().setAdapter(adapter);
         return view;
     }
 }
