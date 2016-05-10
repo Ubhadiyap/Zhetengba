65d528e57540c517854dcbbb6d13c69e18abd0ca
小可 <164454216@qq.com>
2016-05-10 19:01:58 +0800

    //圈子界面，点击分享，弹出dialog

--- app/src/main/java/com/boyuanitsm/zhetengba/adapter/ChanAdapter.java
+++ app/src/main/java/com/boyuanitsm/zhetengba/adapter/ChanAdapter.java
@@ -5,9 +5,11 @@ import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
 
 import com.boyuanitsm.zhetengba.R;
 import com.boyuanitsm.zhetengba.view.CircleImageView;
+import com.boyuanitsm.zhetengba.view.ShareDialog;
 
 /**
  * 频道listview适配器
@@ -35,7 +37,25 @@ public class ChanAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-       View  view=View.inflate(context,R.layout.item_chanle,null);
-        return view;
+       CaViewHolder viewHolder;
+        if (convertView!=null&&convertView.getTag()!=null){
+            viewHolder= (CaViewHolder) convertView.getTag();
+        }else {
+           convertView= View.inflate(context,R.layout.item_chanle,null);
+            viewHolder=new CaViewHolder();
+            viewHolder.ll_share= (LinearLayout) convertView.findViewById(R.id.ll_share);
+            convertView.setTag(viewHolder);
+        }
+        viewHolder.ll_share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShareDialog dialog=new ShareDialog(context);
+                dialog.show();
+            }
+        });
+        return convertView;
+    }
+    class CaViewHolder{
+        private LinearLayout ll_share;
     }
 }
--- app/src/main/java/com/boyuanitsm/zhetengba/fragment/circleFrg/CircleFrg.java
+++ app/src/main/java/com/boyuanitsm/zhetengba/fragment/circleFrg/CircleFrg.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import com.boyuanitsm.zhetengba.R;
 import com.boyuanitsm.zhetengba.activity.CirMessAct;
+import com.boyuanitsm.zhetengba.activity.GlcircleAct;
 
 /**
  * 圈子界面
@@ -80,6 +81,7 @@ public class CircleFrg extends Fragment implements View.OnClickListener{
 
     @Override
     public void onClick(View v) {
+        Intent intent=new Intent();
         FragmentTransaction fragmentTransaction = childFragmentManager.beginTransaction();
         switch (v.getId()){
             case R.id.tv_chanel:
@@ -109,10 +111,12 @@ public class CircleFrg extends Fragment implements View.OnClickListener{
                 fragmentTransaction.commit();
                 break;
             case R.id.iv_quan:
-                Toast.makeText(getContext(),"点击圈",0).show();
+                //跳转至圈子管理
+                intent.setClass(getContext(),GlcircleAct.class);
+                startActivity(intent);
                 break;
             case R.id.iv_newmes:
-                Intent intent=new Intent();
+                //跳转至，圈子消息
                 intent.setClass(getContext(), CirMessAct.class);
                 startActivity(intent);
                 break;
