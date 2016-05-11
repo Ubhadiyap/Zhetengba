555e86372dc28d05a0c333301195ec8ed2f66106
孙少池 <673132032@qq.com>
2016-05-11 18:33:53 +0800

    标签详情

---
+++ app/src/main/java/com/boyuanitsm/zhetengba/activity/circle/CirxqAct.java
@@ -0,0 +1,23 @@
+package com.boyuanitsm.zhetengba.activity.circle;
+
+import android.os.Bundle;
+
+import com.boyuanitsm.zhetengba.R;
+import com.boyuanitsm.zhetengba.base.BaseActivity;
+
+/**
+ * 圈子详情界面
+ * Created by bitch-1 on 2016/5/11.
+ */
+public class CirxqAct extends BaseActivity {
+    @Override
+    public void setLayout() {
+        setContentView(R.layout.act_cirxq);
+
+    }
+
+    @Override
+    public void init(Bundle savedInstanceState) {
+
+    }
+}
