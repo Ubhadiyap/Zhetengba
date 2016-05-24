986da12b81bf4c3c90a53feff59a7b8aeca816a3
孙少池 <673132032@qq.com>
2016-05-24 11:14:16 +0800

    小修改

--- app/src/main/java/com/boyuanitsm/zhetengba/activity/circle/CircleppAct.java
+++ app/src/main/java/com/boyuanitsm/zhetengba/activity/circle/CircleppAct.java
@@ -1,7 +1,5 @@
 package com.boyuanitsm.zhetengba.activity.circle;
 
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -10,7 +8,6 @@ import android.widget.TextView;
 import com.boyuanitsm.zhetengba.R;
 import com.boyuanitsm.zhetengba.adapter.CirpplistAdapter;
 import com.boyuanitsm.zhetengba.base.BaseActivity;
-import com.boyuanitsm.zhetengba.util.ZhetebaUtils;
 import com.boyuanitsm.zhetengba.utils.LayoutHelperUtil;
 import com.boyuanitsm.zhetengba.view.refresh.PullToRefreshListView;
 import com.lidroid.xutils.view.annotation.ViewInject;
@@ -20,7 +17,7 @@ import com.lidroid.xutils.view.annotation.event.OnClick;
  * 圈子成员界面
  * Created by bitch-1 on 2016/5/9.
  */
-public class CircleppAct extends BaseActivity implements View.OnClickListener{
+public class CircleppAct extends BaseActivity {
     @ViewInject(R.id.plv)
     private PullToRefreshListView plv;
     private String tv_right;//标题栏右边文字
@@ -57,10 +54,12 @@ public class CircleppAct extends BaseActivity implements View.OnClickListener{
                 Log.i("hah","gaga");
                 if(flag){
                     flag=false;
-                    tv_gl_member.setText("取消");
+                    tv_gl_member.setText("完成");
+                    setTopTitle("管理圈子");
                     plv.getRefreshableView().setAdapter(new CirpplistAdapter(getApplicationContext(), true));
                 }
                 else {tv_gl_member.setText("管理成员"); flag=true;
+                    setTopTitle("圈子成员");
                       plv.getRefreshableView().setAdapter(new CirpplistAdapter(getApplicationContext(), false));}
                 break;
         }
@@ -68,12 +67,12 @@ public class CircleppAct extends BaseActivity implements View.OnClickListener{
 
     }
 
-    @OnClick({R.id.tv_gl_member})
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.tv_gl_member:
-                openActivity(GLCirAct.class);
-        }
-    }
+//    @OnClick({R.id.tv_gl_member})
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()){
+//            case R.id.tv_gl_member:
+//                openActivity(GLCirAct.class);
+//        }
+//    }
 }
