7758873fa19c0e760b621f18ddc117a268178e2e
王斌 <493204277@qq.com>
2016-05-31 10:14:49 +0800

    请求数据封装

---
+++ app/src/main/java/com/boyuanitsm/zhetengba/http/IZtbUrl.java
@@ -0,0 +1,13 @@
+package com.boyuanitsm.zhetengba.http;
+
+/**
+ * 基础URl
+ * Created by wangbin on 16/5/30.
+ */
+public interface IZtbUrl {
+
+    //基础地址
+    public static final String BASE_URL="";
+
+    public static final String LOGIN_URL=BASE_URL+"";
+}
