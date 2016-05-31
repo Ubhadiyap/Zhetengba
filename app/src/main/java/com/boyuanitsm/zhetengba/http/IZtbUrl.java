34bd113d6175345a3767a21487ec4390d5712c67
孙少池 <673132032@qq.com>
2016-05-31 13:26:19 +0800

    no message

--- app/src/main/java/com/boyuanitsm/zhetengba/http/IZtbUrl.java
+++ app/src/main/java/com/boyuanitsm/zhetengba/http/IZtbUrl.java
@@ -9,5 +9,21 @@ public interface IZtbUrl {
     //基础地址
     public static final String BASE_URL="";
 
-    public static final String LOGIN_URL=BASE_URL+"";
+    /**=======登录地址 =======*/
+    public static final String LOGIN_URL=BASE_URL+"manager/unLogin/login.do";
+
+    /**=========登出地址==========**/
+    public static final String LOGOUT_URL=BASE_URL+"manager/unLogin/logout.do";
+
+    /**=========注册地址==========**/
+    public static final String REGISTER_URL=BASE_URL+"manager/unLogin/register.do";
+
+    /**=========发送验证码地址==========**/
+    public static final String SENDSMSCAPTCHA_URL=BASE_URL+"manager/unLogin/sendSmsCaptcha.do";
+
+    /**=========修改密码地址==========**/
+    public static final String MODIFYUSERPASSWORD_URL=BASE_URL+"manager/login/modifyUserPassword.do";
+
+    /**=========忘记密码地址==========**/
+    public static final String FORGETPASSWORD_URL=BASE_URL+"manager/unLogin/forgetPassword.do";
 }
