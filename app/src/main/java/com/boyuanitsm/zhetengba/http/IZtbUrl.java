942a4527f2350cb53edf7edfc3a2117b6f225fe0
王斌 <493204277@qq.com>
2016-05-31 13:25:03 +0800

    登录

--- app/src/main/java/com/boyuanitsm/zhetengba/http/IZtbUrl.java
+++ app/src/main/java/com/boyuanitsm/zhetengba/http/IZtbUrl.java
@@ -7,7 +7,7 @@ package com.boyuanitsm.zhetengba.http;
 public interface IZtbUrl {
 
     //基础地址
-    public static final String BASE_URL="";
+    public static final String BASE_URL="http://172.16.6.253:8089/zhetengba/";
 
-    public static final String LOGIN_URL=BASE_URL+"";
+    public static final String LOGIN_URL=BASE_URL+"manager/unLogin/login.do";
 }
