36252ded924fdf0125bb2a2e0fdf35eb89824ba5
小可 <164454216@qq.com>
2016-07-06 15:58:15 +0800

    //二维码实体类

---
+++ app/src/main/java/com/boyuanitsm/zhetengba/bean/ErEntity.java
@@ -0,0 +1,26 @@
+package com.boyuanitsm.zhetengba.bean;
+
+/**
+ * 二维码实体
+ * Created by Administrator on 2016/7/6.
+ */
+public class ErEntity {
+    private int type;
+    private String id;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
