a9918d915976cee8ef33f1ce68bec1fe62e63be2
小可 <164454216@qq.com>
2016-07-21 10:48:10 +0800

    //so库缩减，去除不必要的推送so库。

--- app/src/main/java/com/boyuanitsm/zhetengba/adapter/CircleMessAdatper.java
+++ app/src/main/java/com/boyuanitsm/zhetengba/adapter/CircleMessAdatper.java
@@ -121,8 +121,8 @@ public class CircleMessAdatper extends BaseAdapter {
             holder1.bt_no = (Button) convertView.findViewById(R.id.bt_no);
             holder1.tv_shenqing = (TextView) convertView.findViewById(R.id.tv_shenqing);
             holder1.cv_head3 = (CircleImageView) convertView.findViewById(R.id.cv_head3);
-            holder1.tv_creatTime = (TextView) convertView.findViewById(R.id.tv_creatTime);
-            holder1.tv_petName = (TextView) convertView.findViewById(R.id.tv_petName);
+            holder1.tv_creatTime2 = (TextView) convertView.findViewById(R.id.tv_creatTime2);
+            holder1.tv_petName2 = (TextView) convertView.findViewById(R.id.tv_petName2);
            holder1.item_mess= (LinearLayout) convertView.findViewById(R.id.item_mess);
             holder1.item_mess_one=(LinearLayout)convertView.findViewById(R.id.item_mess_one);
             holder1.item_mess_two=(LinearLayout)convertView.findViewById(R.id.item_mess_two);
@@ -144,9 +144,11 @@ public class CircleMessAdatper extends BaseAdapter {
         ImageLoader.getInstance().displayImage(Uitls.imageFullUrl(circleInfoList.get(position).getUserIcon()), holder1.cv_head1, optionsImag);
         if (!TextUtils.isEmpty(circleInfoList.get(position).getPetName())) {
             holder1.niName1.setText(circleInfoList.get(position).getPetName());
+            holder1.tv_petName2.setText(circleInfoList.get(position).getPetName());
         }
         if (!TextUtils.isEmpty(circleInfoList.get(position).getCreateTime())) {
             holder1.createTime1.setText(ZtinfoUtils.timeChange(Long.parseLong(circleInfoList.get(position).getCreateTime())));
+            holder1.tv_creatTime2.setText(ZtinfoUtils.timeChange(Long.parseLong(circleInfoList.get(position).getCreateTime())));
         }
         ImageLoader.getInstance().displayImage(Uitls.imageFullUrl(UserInfoDao.getUser().getIcon()), holder1.iv_icon, optionsImag);
         if (!TextUtils.isEmpty(circleInfoList.get(position).getCommentTalk())) {
@@ -155,13 +157,16 @@ public class CircleMessAdatper extends BaseAdapter {
         } else {
             holder1.tv_talk.setVisibility(View.GONE);
         }
-        if (circleInfoList.get(position).getMessageState().equals(0 + "")) {
-            if (!TextUtils.isEmpty(circleInfoList.get(position).getCommentContent())) {
-                holder1.tv_huifu.setText("评论“我”：" + circleInfoList.get(position).getCommentContent());
+        if (!TextUtils.isEmpty(circleInfoList.get(position).getMsgState())){
+            if (circleInfoList.get(position).getMsgState().equals(0 + "")) {
+                if (!TextUtils.isEmpty(circleInfoList.get(position).getCommentContent())) {
+                    holder1.tv_huifu.setText("评论“我”：" + circleInfoList.get(position).getCommentContent());
+                }
+            } else if (circleInfoList.get(position).getMsgState().equals(1 + "")) {
+                holder1.tv_huifu.setText("赞了“我”的说说!");
             }
-        } else if (circleInfoList.get(position).getMessageState().equals(1 + "")) {
-            holder1.tv_huifu.setText("赞了“我”的说说!");
         }
+
         ImageLoader.getInstance().displayImage(Uitls.imageFullUrl(circleInfoList.get(position).getUserIcon()), holder1.cv_head2, optionsImag);
         if (!TextUtils.isEmpty(circleInfoList.get(position).getPetName())) {
             holder1.tv_petName.setText(circleInfoList.get(position).getPetName());
@@ -194,7 +199,8 @@ public class CircleMessAdatper extends BaseAdapter {
             holder1.bt_yes.setEnabled(true);
             holder1.bt_no.setEnabled(true);
         }
-        if (circleInfoList.get(position).getMessageState().equals(0 + "")) {
+        if (!TextUtils.isEmpty(circleInfoList.get(position).getMsgState())){
+        if (circleInfoList.get(position).getMsgState().equals(0 + "")) {
             holder1.tv_qingqiu.setText(circleInfoList.get(position).getMsgContent());
             final Holder1 finalHolder = holder1;
             holder1.bt_yes.setOnClickListener(new View.OnClickListener() {
@@ -259,7 +265,7 @@ public class CircleMessAdatper extends BaseAdapter {
                 }
             });
 //                        holder2.tv_beizhu.setText("备注：你好，我是李宇春");
-        } else if (circleInfoList.get(position).getMessageState().equals(1 + "")) {
+        } else if (circleInfoList.get(position).getMsgState().equals(1 + "")) {
             holder1.tv_qingqiu.setText(circleInfoList.get(position).getMsgContent());
             final Holder1 finalHolder1 = holder1;
             holder1.bt_yes.setOnClickListener(new View.OnClickListener() {
@@ -316,6 +322,8 @@ public class CircleMessAdatper extends BaseAdapter {
                 }
             });
         }
+        }
+
 
         ImageLoader.getInstance().displayImage(Uitls.imageFullUrl(circleInfoList.get(position).getUserIcon()), holder1.cv_head3, optionsImag);
         if (!TextUtils.isEmpty(circleInfoList.get(position).getPetName())) {
@@ -332,15 +340,18 @@ public class CircleMessAdatper extends BaseAdapter {
                 context.startActivity(intent);
             }
         });
-        if (circleInfoList.get(position).getMessageState().equals(0 + "")) {
-            holder1.tv_shenqing.setText("同意了我的请求，已加入" + circleInfoList.get(position).getCircleName());
-        } else if (circleInfoList.get(position).getMessageState().equals(1 + "")) {
-            holder1.tv_shenqing.setText("拒绝了我的请求，不参加" + circleInfoList.get(position).getCircleName());
-        } else if (circleInfoList.get(position).getMessageState().equals(2 + "")) {
-            holder1.tv_shenqing.setText("同意了我的邀请，已加入" + circleInfoList.get(position).getCircleName());
-        } else if (circleInfoList.get(position).getMessageState().equals(3 + "")) {
-            holder1.tv_shenqing.setText("拒绝了我的邀请，不参加" + circleInfoList.get(position).getCircleName());
+        if (!TextUtils.isEmpty(circleInfoList.get(position).getMsgState())){
+            if (circleInfoList.get(position).getMsgState().equals(0 + "")) {
+                holder1.tv_shenqing.setText("同意了我的请求，已加入" + circleInfoList.get(position).getCircleName());
+            } else if (circleInfoList.get(position).getMessageState().equals(1 + "")) {
+                holder1.tv_shenqing.setText("拒绝了我的请求，不参加" + circleInfoList.get(position).getCircleName());
+            } else if (circleInfoList.get(position).getMessageState().equals(2 + "")) {
+                holder1.tv_shenqing.setText("同意了我的邀请，已加入" + circleInfoList.get(position).getCircleName());
+            } else if (circleInfoList.get(position).getMessageState().equals(3 + "")) {
+                holder1.tv_shenqing.setText("拒绝了我的邀请，不参加" + circleInfoList.get(position).getCircleName());
+            }
         }
+
 
         return convertView;
     }
