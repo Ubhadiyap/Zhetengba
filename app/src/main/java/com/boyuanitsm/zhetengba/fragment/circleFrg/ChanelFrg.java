9e5bf16d11dca4cdee7db3c975f444ddc4fa781e
小可 <164454216@qq.com>
2016-08-10 13:49:36 +0800

    //广场优化

--- app/src/main/java/com/boyuanitsm/zhetengba/fragment/circleFrg/ChanelFrg.java
+++ app/src/main/java/com/boyuanitsm/zhetengba/fragment/circleFrg/ChanelFrg.java
@@ -30,6 +30,7 @@ import com.boyuanitsm.zhetengba.utils.ACache;
 import com.boyuanitsm.zhetengba.utils.MyLogUtils;
 import com.boyuanitsm.zhetengba.utils.ZtinfoUtils;
 import com.boyuanitsm.zhetengba.view.bounScrollView.BounceScrollView;
+import com.boyuanitsm.zhetengba.view.bounScrollView.ViewPagerIndicator;
 import com.boyuanitsm.zhetengba.view.refresh.PullToRefreshListView;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -44,7 +45,7 @@ import java.util.List;
  * Created by xiaoke on 2016/5/2.
  */
 public class ChanelFrg extends BaseFragment implements View.OnClickListener {
-    private int currentPos;//当前位置
+    private int currentPos=0;//当前位置
     private int pos = 0;//返回的位置
     private int mTitleMargin;//头部标签之间空隙；
     private int page = 1;
@@ -52,7 +53,7 @@ public class ChanelFrg extends BaseFragment implements View.OnClickListener {
     private View view;//当前view
     private PullToRefreshListView vp_chan;//viewpager
     private ChanAdapter adapter;
-    private LinearLayout titleLayout;//频道，头部标签布局
+    private ViewPagerIndicator titleLayout;//频道，头部标签布局
     private ArrayList<TextView> textViewList;//承载标签的TextView集合
     private List<UserInterestInfo> titleList;//标签集合
     private ArrayList<Integer> moveToList;//设置textview宽高集合
@@ -84,7 +85,7 @@ public class ChanelFrg extends BaseFragment implements View.OnClickListener {
     }
 
     private void initView() {
-        titleLayout = (LinearLayout) view.findViewById(R.id.titleLayout);
+        titleLayout = (ViewPagerIndicator) view.findViewById(R.id.titleLayout);
 //        vp_chan = (PullToRefreshListView) view.findViewById(R.id.vp_chan);
         viewPager = (ViewPager) view.findViewById(R.id.vp_chanel);
         hslv_chanel = (BounceScrollView) view.findViewById(R.id.hslv_chanel);
@@ -97,102 +98,26 @@ public class ChanelFrg extends BaseFragment implements View.OnClickListener {
      * 填充数据
      */
     private void initDate(final List<UserInterestInfo> titleList) {
-        MyLogUtils.info(currentPos + "刷新位置信息。。。。");
-        textViewList = new ArrayList<>();
-        moveToList = new ArrayList<>();
-        fragments = new ArrayList<>();
-        if (titleList.size() <= currentPos) {
-            currentPos = 0;
-        } else {
-            currentPos = pos;
-        }
-        if (titleList.size() > 0) {
-            ChanelPageAdapter pageAdapter = new ChanelPageAdapter(getChildFragmentManager(), mActivity, titleList, currentPos);
+        titleLayout.setTabItemTitles(titleList);
+        ChanelPageAdapter pageAdapter = new ChanelPageAdapter(getChildFragmentManager(), mActivity, titleList, currentPos);
             viewPager.setAdapter(pageAdapter);
-            for (int i = 0; i < titleList.size(); i++) {
-                addTitleLayout(titleList.get(i).getDictName(), i);
+        titleLayout.setViewPager(viewPager, hslv_chanel, currentPos);
+        titleLayout.setOnPageChangeListener(new ViewPagerIndicator.PageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
             }
-            if (textViewList != null && textViewList.size() > 0) {
-                textViewList.get(currentPos).setTextColor(Color.parseColor("#52C791"));//默认加载项，标签文字对应变色
+
+            @Override
+            public void onPageSelected(int position) {
+                currentPos=position;
             }
-            viewPager.setCurrentItem(currentPos);//默认加载条目
-            viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {//滑动监听
-                @Override
-                public void onPageScrolled(int i, float v, int i1) {
 
-                }
+            @Override
+            public void onPageScrollStateChanged(int state) {
 
-                @Override
-                public void onPageSelected(int i) {
-                    //当前位置textview 文字选中变色
-                    textViewList.get(currentPos).setTextColor(Color.parseColor("#999999"));
-                    textViewList.get(i).setTextColor(Color.parseColor("#52C791"));
-                    currentPos = i;
-                    if (i == preItem) {
-                        //不作处理
-                        MyLogUtils.degug("没有移动");
-                        return;
-                    }
-                    if (i > preItem) {
-                        //从左向右滑
-                        MyLogUtils.degug("从左向右滑");
-                        if (i > titleList.size()/2) {
-                            hslv_chanel.scrollTo((int) moveToList.get(i), 0);
-                        }
-                        preItem = i;
-                        return;
-                    }
-                    if (i < preItem) {
-                        //从右向左滑
-                        if (i < titleList.size()/2) {
-                            hslv_chanel.scrollTo((int) moveToList.get(i), 0);
-                        }
-                        MyLogUtils.degug("从右向左滑");
-                        preItem = i;
-                        return;
-                    }
-                }
-
-                @Override
-                public void onPageScrollStateChanged(int i) {
-
-                }
-            });
-        }
-    }
-
-    /***
-     * 填充titleLayout
-     *
-     * @param title
-     * @param position
-     */
-
-    private void addTitleLayout(String title, int position) {
-        final TextView textView = (TextView) mActivity.getLayoutInflater().inflate(R.layout.chanel_child_title, null);
-        textView.setText(title);
-        textView.setTextSize(14);
-        textView.setTextColor(Color.parseColor("#999999"));
-        textView.setTag(position);//设置position Tag
-        MyLogUtils.info("title：位置" + title + ":" + position);
-        textView.setOnClickListener(new posOnClickListener());
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-        mTitleMargin = ZtinfoUtils.dip2px(mActivity, 5);        //设置左右间隙
-        params.leftMargin = ZtinfoUtils.dip2px(mActivity, mTitleMargin);
-        params.rightMargin = ZtinfoUtils.dip2px(mActivity, mTitleMargin);
-        titleLayout.addView(textView, params);
-        textViewList.add(textView);
-        int width;        //设置宽高
-        if (position == 0) {
-            width = 0;
-            moveToList.add(width);
-        } else {
-            int w = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
-            int h = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
-            textViewList.get(position - 1).measure(w, h);
-            width = textViewList.get(position - 1).getMeasuredWidth() + mTitleMargin * 4;
-            moveToList.add(width + moveToList.get(moveToList.size() - 1));
-        }
+            }
+        });
     }
 
 
@@ -214,22 +139,6 @@ public class ChanelFrg extends BaseFragment implements View.OnClickListener {
         }
     }
 
-    class posOnClickListener implements View.OnClickListener {
-
-        @Override
-        public void onClick(View view) {
-            MyLogUtils.degug("传过来的位置：" + currentPos + "====点击位置：" + (int) view.getTag());
-            if ((int) view.getTag() == currentPos) {
-                return;
-            }
-            textViewList.get(currentPos).setTextColor(Color.parseColor("#999999"));
-            currentPos = (int) view.getTag();
-            page = 1;
-            rows = 10;
-            viewPager.setCurrentItem(currentPos);
-            textViewList.get(currentPos).setTextColor(Color.parseColor("#52C791"));
-        }
-    }
 
     /**
      * 获取我的兴趣标签
