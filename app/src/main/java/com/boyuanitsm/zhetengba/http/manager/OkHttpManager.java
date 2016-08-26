f3d01e6f1fb8764e59d37f99cb1a69fa5669e5eb
王斌 <493204277@qq.com>
2016-08-26 14:09:37 +0800

    去掉表情的说明，修改通知栏提示

--- app/src/main/java/com/boyuanitsm/zhetengba/http/manager/OkHttpManager.java
+++ app/src/main/java/com/boyuanitsm/zhetengba/http/manager/OkHttpManager.java
@@ -1,5 +1,7 @@
 package com.boyuanitsm.zhetengba.http.manager;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.text.TextUtils;
 
 import com.boyuanitsm.zhetengba.MyApplication;
@@ -33,11 +35,14 @@ public class OkHttpManager {
     private Headers headers;
     private static OkHttpManager mInstance;
     private Gson mGson;
+    private Handler mDelivery;
 
-    private OkHttpManager(){
-        mGson=new Gson();
+    private OkHttpManager() {
+        mGson = new Gson();
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
         mOkHttpClient = builder.build();
+        //cookie enabled
+        mDelivery = new Handler(Looper.getMainLooper());
     }
 
     public static OkHttpManager getInstance() {
@@ -52,16 +57,13 @@ public class OkHttpManager {
     }
 
 
-
-    private Param[] map2Params(Map<String, String> params)
-    {
+    private Param[] map2Params(Map<String, String> params) {
         if (params == null) return new Param[0];
         int size = params.size();
         Param[] res = new Param[size];
         Set<Map.Entry<String, String>> entries = params.entrySet();
         int i = 0;
-        for (Map.Entry<String, String> entry : entries)
-        {
+        for (Map.Entry<String, String> entry : entries) {
             res[i++] = new Param(entry.getKey(), entry.getValue());
         }
         return res;
@@ -74,61 +76,89 @@ public class OkHttpManager {
      * @param paramMap
      * @return
      */
-    public void doPost(String url,Map<String, String> paramMap,final ResultCallback callback) {
+    public void doPost(String url, Map<String, String> paramMap, final ResultCallback callback) {
         headers = HttpHeaderHelper.getHeaders();
-            MyLogUtils.info("请求地址:"+url);
-        MyLogUtils.info("请求参数:"+ GsonUtils.bean2Json(paramMap));
-            FormBody.Builder parmBuilder = new FormBody.Builder();
-            Param[] params = map2Params(paramMap);
-            if (params != null && params.length > 0) {
-                for (Param param : params) {
-                    parmBuilder.add(param.key, param.value);
-                }
+        MyLogUtils.info("请求地址:" + url);
+        MyLogUtils.info("请求参数:" + GsonUtils.bean2Json(paramMap));
+        FormBody.Builder parmBuilder = new FormBody.Builder();
+        Param[] params = map2Params(paramMap);
+        if (params != null && params.length > 0) {
+            for (Param param : params) {
+                parmBuilder.add(param.key, param.value);
+            }
+        }
+
+        Request.Builder builder = new Request.Builder();
+        builder.url(url).post(parmBuilder.build());
+        if (headers != null) {
+            builder.headers(headers);
+        }
+
+        Request request = builder.build();
+        mOkHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                sendFailedStringCallback(-1, "失败", callback);
+//                callback.onError(-1, "失败");
             }
 
-            Request.Builder builder = new Request.Builder();
-            builder.url(url).post(parmBuilder.build());
-            if (headers != null) {
-                builder.headers(headers);
-            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                //获取cookie
+                String header = response.header("set-cookie");
+                MyLogUtils.info("获取到cookie:" + header);
+                if (!TextUtils.isEmpty(header))
+                    SpUtils.setCooike(MyApplication.getInstance(), header);
+                final String result = response.body().string();
+                MyLogUtils.info("获取result：" + result);
+                if (callback.mType == String.class) {
 
-            Request request = builder.build();
-            mOkHttpClient.newCall(request).enqueue(new Callback() {
-                @Override
-                public void onFailure(Call call, IOException e) {
-                    callback.onError(-1,"失败");
-                }
-
-                @Override
-                public void onResponse(Call call, Response response) throws IOException {
-                    //获取cookie
-                    String header = response.header("set-cookie");
-                    MyLogUtils.info("获取到cookie:"+header);
-                    if (!TextUtils.isEmpty(header))
-                        SpUtils.setCooike(MyApplication.getInstance(), header);
-                    final String result = response.body().string();
-                    MyLogUtils.info("获取result："+result);
-                    if (callback.mType == String.class) {
-                        callback.onResponse(result);
-                    } else {
-                        try {
-                            JSONObject jsonObject = new JSONObject(result);
-                            int status = jsonObject.getInt("status");
-                            if (status == 200) {//成功
-                                Object o = mGson.fromJson(result, callback.mType);
-                                callback.onResponse(o);
-                            } else if(status==601){
-                                callback.onError(status, result);
-                            }else {//失败
-                                callback.onError(status, jsonObject.getString("message"));
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
+//                        callback.onResponse(result);
+                    sendSuccessResultCallback(result, callback);
+                } else {
+                    try {
+                        JSONObject jsonObject = new JSONObject(result);
+                        int status = jsonObject.getInt("status");
+                        if (status == 200) {//成功
+                            Object o = mGson.fromJson(result, callback.mType);
+                            sendSuccessResultCallback(o, callback);
+//                                callback.onResponse(o);
+                        } else if (status == 601) {
+                            sendFailedStringCallback(status, result, callback);
+//                                callback.onError(status, result);
+                        } else {//失败
+                            sendFailedStringCallback(status, jsonObject.getString("message"), callback);
+//                                callback.onError(status, jsonObject.getString("message"));
                         }
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
                 }
-            });
+            }
+        });
 
+    }
+
+
+    private void sendFailedStringCallback(final int status, final String e, final ResultCallback callback) {
+        mDelivery.post(new Runnable() {
+            @Override
+            public void run() {
+                if (callback != null)
+                    callback.onError(status, e);
+            }
+        });
+    }
+
+    private void sendSuccessResultCallback(final Object object, final ResultCallback callback) {
+        mDelivery.post(new Runnable() {
+            @Override
+            public void run() {
+                if (callback != null) {
+                    callback.onResponse(object);
+                }
+            }
+        });
     }
 
 
