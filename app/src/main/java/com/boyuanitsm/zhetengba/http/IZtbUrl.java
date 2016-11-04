fa49278847e28673c5a3eeeed876b6002a9e1398
小可 <164454216@qq.com>
2016-11-04 18:50:04 +0800

    //地址不变。

--- app/src/main/java/com/boyuanitsm/zhetengba/http/IZtbUrl.java
+++ app/src/main/java/com/boyuanitsm/zhetengba/http/IZtbUrl.java
@@ -10,11 +10,12 @@ public interface IZtbUrl {
     //基础地址
     //外网，h5web，更换domain地址
 //    public static final String BASE_URL = "http://hseb.net.cn/zhetengba/";
-//  public static final String BASE_URL="http://180.76.149.156:8033/zhetengba/";
+  public static final String BASE_URL="http://180.76.149.156:8033/zhetengba/";
 
 
     //开发
-    public static final String BASE_URL = "http://172.16.6.253:8089/zhetengba/";
+//        public static final String BASE_URL = "http://172.16.6.146:8080/zhetengba/";
+//    public static final String BASE_URL = "http://172.16.6.253:8089/zhetengba/";
 //    public static final String BASE_URL = "http://180.76.149.156:8060/zhetengba/";
 
 
