646e56ccc312b3f2d7653c3434e9e82d5151365b
小可 <164454216@qq.com>
2016-12-05 14:12:06 +0800

    //切换分支。

--- app/src/main/java/com/boyuanitsm/zhetengba/http/IZtbUrl.java
+++ app/src/main/java/com/boyuanitsm/zhetengba/http/IZtbUrl.java
@@ -14,8 +14,8 @@ public interface IZtbUrl {
 
 
     //开发
-    public static final String BASE_URL="http://180.167.77.62:8081/zhetengba/";
-//    public static final String BASE_URL = "http://172.16.6.253:8089/zhetengba/";
+//    public static final String BASE_URL="http://180.167.77.62:8081/zhetengba/";
+    public static final String BASE_URL = "http://172.16.6.253:8089/zhetengba/";
 //    public static final String BASE_URL = "http:/172.16.5.133:8090/zhetengba/";
 
 //    public static final String BASE_URL = "http://180.76.149.156:8060/zhetengba/";
